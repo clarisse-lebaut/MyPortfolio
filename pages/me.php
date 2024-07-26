af23420070d4e8a55850398a667d4a7290187f62
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-07-26 22:47:51 +0200

    add cv bd

--- pages/me.php
+++ pages/me.php
@@ -1,9 +1,20 @@
+<link rel="stylesheet" href="../assets/styles/global.css">
+<link rel="stylesheet" href="../assets/styles/me.css">
+
 <?php
-include "../components/header.php";
+include "./components/header.php";
 ?>
 
-<h1>me</h1>
-
+<h1 class="title-page-projects">LAISSER MOI VOUS RACONTER UNE HISTOIRE ...</h1>
+    <main>
+      <section class="main container mt-5">
+        <div class="strip1"></div>
+        <div class="strip2"></div>
+        <div class="strip3"></div>
+        <div class="strip4"></div>
+        <div class="strip5"></div>
+      </section>
+    </main>
 <?php
-include "../components/footer.php";
+include "./components/footer.php";
 ?>
