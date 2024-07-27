70eacab1086f21d6773f83966c246aa2f4ca62d8
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-07-27 11:31:25 +0200

    add css on title

--- pages/me.php
+++ pages/me.php
@@ -6,15 +6,7 @@ include "./components/header.php";
 ?>
 
 <h1 class="title-page-projects">LAISSER MOI VOUS RACONTER UNE HISTOIRE ...</h1>
-    <main>
-      <section class="main container mt-5">
-        <div class="strip1"></div>
-        <div class="strip2"></div>
-        <div class="strip3"></div>
-        <div class="strip4"></div>
-        <div class="strip5"></div>
-      </section>
-    </main>
+
 <?php
 include "./components/footer.php";
 ?>
