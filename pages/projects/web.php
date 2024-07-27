33953f810c992e8d40c195c6641762492af2ef51
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-07-27 11:46:23 +0200

    add cards for each project

--- pages/projects/web.php
+++ pages/projects/web.php
@@ -1,68 +1,39 @@
+<title>Mes Projets Web</title>
 <link rel="stylesheet" href="../assets/styles/global.css">
 <link rel="stylesheet" href="../assets/styles/projects.css">
 
-<?php
-include "./components/header.php";
-?>
+
+<?php include "./components/header.php"; ?>
 
 <main>
-
-<h1 class="title-page-projects">MES PROJETS : WEB</h1>
-
-<section class="main-container-web">
-    <div class="card-container">
-        <a href="index.php?page=watchme"><img class="card-pic" src="../assets/img/category/coding.jpeg" alt=""></a>
-        <p class="card-text">WatchMe</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-    <div class="card-container">
-        <a href="index.php?page=watchme"><img class="card-pic" src="../assets/img/category/coding.jpeg" alt=""></a>
-        <p class="card-text">MatchaTea</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-    <div class="card-container">
-        <a href="index.php?page=watchme"><img class="card-pic" src="../assets/img/category/coding.jpeg" alt=""></a>
-        <p class="card-text">Clicker</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-    <div class="card-container">
-        <a href="index.php?page=watchme"><img class="card-pic" src="../assets/img/category/coding.jpeg" alt=""></a>
-        <p class="card-text">Memory</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-    <div class="card-container">
-        <img class="card-pic" src="../assets/img/category/coding.jpeg" alt="">
-        <p class="card-text">CineTech</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-    <div class="card-container">
-        <img class="card-pic" src="../assets/img/category/coding.jpeg" alt="">
-        <p class="card-text">BloomPetal</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-    <div class="card-container">
-        <img class="card-pic" src="../assets/img/category/coding.jpeg" alt="">
-        <p class="card-text">Password Generator</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-    <div class="card-container">
-        <img class="card-pic" src="../assets/img/category/coding.jpeg" alt="">
-        <p class="card-text">Quiz Night</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-    <div class="card-container">
-        <img class="card-pic" src="../assets/img/category/coding.jpeg" alt="">
-        <p class="card-text">CandyShop</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-    <div class="card-container">
-        <img class="card-pic" src="../assets/img/category/coding.jpeg" alt="">
-        <p class="card-text">JPO-Connect</p>
-        <p class="card-description">Un site de montre</p>
-    </div>
-</section>
+    <h1 class="title-page-projects">MES PROJETS : WEB</h1>
+    <section class="main-container-web" id="projects-container">
+        <!-- Les cartes de projet seront ajoutées ici par JavaScript -->
+    </section>
 </main>
 
-<?php
-include "./components/footer.php";
-?>
+<?php include "./components/footer.php"; ?>
+
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        fetch('../assets/db/database-web.json')
+            .then(response => response.json())
+            .then(data => {
+                const container = document.getElementById('projects-container');
+                data.forEach(project => {
+                    const card = document.createElement('div');
+                    card.className = 'card-container';
+                    card.innerHTML = `
+                        <a href="${project.link}"><img class="card-pic" src="${project.image}" alt="${project.title}"></a>
+                        <p class="card-text">${project.title}</p>
+                        <p class="card-description">${project.description}</p>
+                    `;
+                    container.appendChild(card);
+                });
+            })
+            .catch(error => console.error('Error fetching the projects:', error));
+    });
+</script>
+
+</body>
+</html>
