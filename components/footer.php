8d1a212771a79e8f6da871b92198ed87ff9ecfc6
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-07-29 02:51:06 +0200

    update all link

--- components/footer.php
+++ components/footer.php
@@ -4,36 +4,36 @@
     <section class="section-link">
         <div>
             <p>Navigation</p>
-            <li class="main-footer-li"><a class="main-footer-a" href="./index.php?pages=home">Home</a></li>
-            <li class="main-footer-li"><a class="main-footer-a" href="./index.php?pages=home">Mes projets</a></li>
-            <li class="main-footer-li"><a class="main-footer-a" href="./index.php?pages=home">Qui suis-je ?</a></li>
-            <li class="main-footer-li"><a class="main-footer-a" href="./index.php?pages=home">Contact</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="../index.php?page=home">Home</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="../index.php?page=projects">Mes projets</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="../index.php?page=me">Qui suis-je ?</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="../index.php?page=contact">Contact</a></li>
         </div>
         <div>
             <p>Mes projets</p>
-            <li class="main-footer-li"><a class="main-footer-a" href="index.php?pages=home">Arts</a></li>
-            <li class="main-footer-li"><a class="main-footer-a" href="index.php?pages=home">Informatique</a></li>
-            <li class="main-footer-li"><a class="main-footer-a" href="index.php?pages=home">Musique</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=graphic">Arts</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=web">Informatique</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=music">Musique</a></li>
         </div>
         <div>
             <p>Qui suis-je ?</p>
-            <li class="main-footer-li"><a class="main-footer-a" href="index.php?pages=home">Présentation</a></li>
-            <li class="main-footer-li"><a class="main-footer-a" href="index.php?pages=home">Mes formations</a></li>
-            <li class="main-footer-li"><a class="main-footer-a" href="index.php?pages=home">Mes expériences</a></li>
-            <li class="main-footer-li"><a class="main-footer-a" href="index.php?pages=home">Mes compétences</a></li>
-            <li class="main-footer-li"><a class="main-footer-a" href="index.php?pages=home">Mon CV</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=me">Présentation</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=me">Mes formations</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=me">Mes expériences</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=me">Mes compétences</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=CV">Mon CV</a></li>
         </div>
         <div>
             <p>A propos</p>
-            <li class="main-footer-li"><a class="main-footer-a" href="index.php?pages=home">Mon portfolio</a></li>
+            <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=detail">Mon portfolio</a></li>
         </div>
     </section>
     <section class="section-logo">
-        <a href="">
+        <a href="https://linkedin.com/in/clarisse-le-baut-161499235" target="_blank">
             <img class="icon" src="../assets/img/icons/linkedin.svg" alt="logo linkedin" /></a>
-        <a href="">
-            <img class="icon" src="../assets/img/icons/square-envelope-solid.svg" alt="logo mail" /></a>
-        <a class="link-footer" href="">
+        <a href="" target="_blank">
+            <img class="icon" src="../assets/img/icons/square-envelope-solid.svg" alt="logo mail" target="_blank" /></a>
+        <a class="link-footer" href="https://github.com/clarisse-lebaut" target="_blank">
             <img class="icon" src="../assets/img/icons/square-github.svg" alt="logo github" /></a>
     </section>
 </nav>
