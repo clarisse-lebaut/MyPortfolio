d90a68ca1ea5195c50441cb26e91037e1fba9452
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-08-03 15:40:43 +0200

    star home page and me page

--- pages/home.php
+++ pages/home.php
@@ -5,3 +5,14 @@
 include "./components/header.php";
 ?>
 
+<h1 class="title-page-projects">Bienvenue, bienvenue !</h1>
+<p class="title-page-projects">Qu'allez-vous trouvez ici ? Mon unviers qu'on espère bien rangé et attrayant !</p>
+<p class="title-page-projects">Apprendre est un raison de vivre. Il y a tellement de choses qui compose notre monde et tellement de sujet à découvir que je ne sais plus ou donner de la tête.
+    J'ai plusieurs centre d'intêret et souhaite vivement participer à la création et l'innovation de tout ce qui est possible de rélaiser dans ce monde.
+    Je vous laisse découvrir une ébauche d'un unviers qui est le mien. Experte en rien certe, mais curieuse de tout, mon profil est souvent dit atypique. Je préfère le voir sans limites !
+</p>
+<section class="presentation">
+    <img class="avatar" src="../assets/img/bimoji/hero.png" alt="avatar">
+    <img class="avatar" src="../assets/img/bimoji/hero.png" alt="avatar">
+    <img class="avatar" src="../assets/img/bimoji/hero.png" alt="avatar">
+</section>
