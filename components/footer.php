91423d8e83c94292888c638063a185074efc8b65
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-08-08 11:20:22 +0200

    last update befor to get on my laptop

--- components/footer.php
+++ components/footer.php
@@ -3,20 +3,20 @@
 <nav class="main-footer-nav">
     <section class="section-link">
         <div>
-            <p class="title-footer-box">Navigation</p>
+            <p class="title-footer-box"><b>NAVIGATION</b></p>
             <li class="main-footer-li"><a class="main-footer-a" href="../index.php?page=home">Home</a></li>
             <li class="main-footer-li"><a class="main-footer-a" href="../index.php?page=projects">Mes projets</a></li>
             <li class="main-footer-li"><a class="main-footer-a" href="../index.php?page=me">Qui suis-je ?</a></li>
             <li class="main-footer-li"><a class="main-footer-a" href="../index.php?page=about">A Propos</a></li>
         </div>
         <div>
-            <p class="title-footer-box">Mes projets</p>
+            <p class="title-footer-box"><b>MES PROJETS</b></p>
             <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=web">Web</a></li>
             <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=music">Musicaux</a></li>
             <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=graphic">Grapiques</a></li>
         </div>
         <div>
-            <p class="title-footer-box">Qui suis-je ?</p>
+            <p class="title-footer-box"><b>QUI SUIS-JE ?</b></p>
             <li class="main-footer-li"><a class="main-footer-a" href="index.php?page=me">Présentation</a></li>
             <li class="main-footer-li"><a class="main-footer-a" href="files/document.pdf" target="_blank">Mon CV</a></li>
         </div>
