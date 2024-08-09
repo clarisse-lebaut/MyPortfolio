253c1ab45213bc3c236acfd98b047ae8ed9e7cd6
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-08-09 15:32:59 +0200

    responsif for nav-project

--- components/nav-projects.php
+++ components/nav-projects.php
@@ -3,28 +3,42 @@
 <link rel="stylesheet" href="../assets/styles/nav-projects.css">
 
 <section class="main-container">
-    <div class="container">
-      <a href="index.php?page=web">
-        <div class="content">
-          <img class="pic" src="../assets/img/category/coding.jpeg" alt="">
-          <p class="text">Web</p>
-        </div>
-      </a>
-    </div>
-    <div class="container">
-      <a href="index.php?page=music">
-        <div class="content">
-          <img class="pic" src="../assets/img/category/musique.jpg" alt="">
-          <p class="text">Musicaux</p>
-        </div>
-      </a>
-    </div>
-    <div class="container">
-      <a href="index.php?page=graphic">
-        <div class="content">
-          <img class="pic" src="../assets/img/category/art.webp" alt="">
-          <p class="text">Graphiques</p>
-        </div>
-      </a>
-    </div>
- </section>
+  <div class="container">
+    <a href="index.php?page=web">
+      <div class="content">
+        <img class="pic" src="../assets/img/category/coding.jpeg" alt="">
+        <p class="text">Web</p>
+      </div>
+    </a>
+  </div>
+  <div class="container">
+    <a href="index.php?page=music">
+      <div class="content">
+        <img class="pic" src="../assets/img/category/musique.jpg" alt="">
+        <p class="text">Musicaux</p>
+      </div>
+    </a>
+  </div>
+  <div class="container">
+    <a href="index.php?page=graphic">
+      <div class="content">
+        <img class="pic" src="../assets/img/category/art.webp" alt="">
+        <p class="text">Graphiques</p>
+      </div>
+    </a>
+  </div>
+</section>
+
+<section class="main-container-mobile">
+  <div class="container-mobile">
+    <a class="a-mobile" href="index.php?page=web">
+      <p class="text-mobile">Web</p>
+    </a>
+    <a class="a-mobile" href="index.php?page=music">
+      <p class="text-mobile">Musicaux</p>
+    </a>
+    <a class="a-mobile" href="index.php?page=graphic">
+      <p class="text-mobile">Graphique</p>
+    </a>
+  </div>
+</section>
