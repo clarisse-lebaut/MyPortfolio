e035e27f758becabdb3e6aec24ae1a95a92ef3fa
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-08-09 15:20:12 +0200

    start responsif,  end : home, main project, header, footer

--- components/header.php
+++ components/header.php
@@ -1,8 +1,17 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <link rel="stylesheet" href="../assets/styles/header.css">
 
 <nav class="main-nav">
-    <li class="main-li"><a class="main-a" href="../index.php?page=home">Home</a></li>
-    <li class="main-li"><a class="main-a" href="../index.php?page=projects">Mes projets</a></li>
-    <li class="main-li"><a class="main-a" href="../index.php?page=me">Qui suis-je ?</a></li>
+    <input type="checkbox" id="menu-toggle" class="menu-toggle">
+    <label for="menu-toggle" class="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+    </label>
+    <div class="menu">
+        <li class="main-li"><a class="main-a" href="../index.php?page=home">Home</a></li>
+        <li class="main-li"><a class="main-a" href="../index.php?page=projects">Mes projets</a></li>
+        <li class="main-li"><a class="main-a" href="../index.php?page=me">Qui suis-je ?</a></li>
+    </div>
 </nav>
-
--- components/nav-projects.php
+++ components/nav-projects.php
@@ -1,3 +1,5 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <link rel="stylesheet" href="../assets/styles/nav-projects.css">
 
 <section class="main-container">
