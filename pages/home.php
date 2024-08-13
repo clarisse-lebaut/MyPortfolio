9f11e6a06e1051de0249faea993ae5cd7ae5db96
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-08-13 21:57:18 +0200

    add readme

--- pages/home.php
+++ pages/home.php
@@ -1,9 +1,10 @@
 <link rel="stylesheet" href="../assets/styles/global.css">
 <link rel="stylesheet" href="../assets/styles/home.css">
 
-<h1 class="title-page-projects">Bienvenue, bienvenue !</h1>
+<h1 class="title-page-projects">Bienvenue sur mon Portfolio !</h1>
 <h2 class="title-page-projects">Que peut-on trouver ici?</h2>
 <h2 class="title-page-projects">Un unviers plus vaste à l'intérieur dont la seule limite est l'imagination !</h2>
+<h2 class="title-page-projects">J'espère que vous trouverez mes projets intéressants.<br>Merci pour votre visite !</h2>
 <div class="container-avatar">
     <img class="avatar" src="../assets/img/bimoji/hey.png" alt="avatar">
 </div>
