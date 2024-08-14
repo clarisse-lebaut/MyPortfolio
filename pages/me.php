32ffdfc79f603303ee91a23356a68138e00d2753
clarisse-lebaut <clarisse.le-baut@laplateforme.io>
2024-08-14 14:02:17 +0200

    udpates experience details

--- pages/me.php
+++ pages/me.php
@@ -17,7 +17,7 @@ include "./components/header.php";
         target="_blank">clarisse.lebaut@gmail.com</a></li>
     <li class="cv-li"><a class="a-contact"
         href="https://www.google.fr/maps/place/13920+Saint-Mitre-les-Remparts/@43.4565357,4.9946024,14z/data=!3m1!4b1!4m6!3m5!1s0x12b61c2c6ec329c1:0x40819a5fd96ff70!8m2!3d43.4376515!4d5.0368613!16s%2Fm%2F02rpcf0?entry=ttu"
-        target="_blank">13920, Saint Mitre les Remparts</li>
+        target="_blank"></a>13920, Saint Mitre les Remparts</li>
     <li class="cv-li">Permis B</li>
     <li class="cv-li"><a class="a-contact" href="files/cv.pdf" target="_blank">Mon CV</a></li>
   </div>
@@ -38,9 +38,9 @@ include "./components/header.php";
               src="../assets/img/icons/square-github.svg" alt=""></a>
         </section>
         <p>
-          Créative avec un parcours atypique, je suis constamment prête à relever de nouveaux défis. 
-          L'innovation est au cœur de ma démarche, et je m'efforce d'apporter des idées novatrices pour créer des outils originaux. 
-          Bien que je n'aie pas de spécialité unique, ma curiosité insatiable me pousse à explorer divers domaines. 
+          Créative avec un parcours atypique, je suis constamment prête à relever de nouveaux défis.
+          L'innovation est au cœur de ma démarche, et je m'efforce d'apporter des idées novatrices pour créer des outils originaux.
+          Bien que je n'aie pas de spécialité unique, ma curiosité insatiable me pousse à explorer divers domaines.
           Pour me décrire, je dirais que je suis une personne dont les limites sont sans cesse en expansion !
         </p>
       </div>
@@ -156,20 +156,55 @@ include "./components/header.php";
       <div class="card-experience">
         <p class="date-experience"><b>Janvier - Août 2022</b></p>
         <p class="titre-experience"><b>Service Civique à Pôle Emploi</b><br>Martigues (13500)</p>
+        <p><b>Contexte de la mission</b></p>
+        <p>
+          Le service civique propose à des volontaires de s'engager activement dans des missions d'intérêt général,
+          couvrant divers domaines tels que l'éducation, la protection de l'environnement, la culture, la solidarité, le sport, ainsi que l'aide humanitaire.
+        </p>
+        <p><b>Missions</b></p>
+        <p>
+          Aider les usagers de Pôle Emploi à devenir autonomes dans l'utilisation des outils numériques et réduire la fracture numérique
+          parmi les différents publics. Accompagner les personnes réfractarire à l'informatique afin de les rendre autonomes,
+          faciliter leur recherche d'emploi, et promouvoir l'utilisation des applications mobiles.
+        </p>
+        <p><b>Tâches</b></p>
         <ul>
-          <li>Aide à l’insertion des usagers en informatique et numérique actualisation</li>
-          <br>
           <li>(Ré)Inscription</li>
           <br>
           <li>Prise RDV</li>
           <br>
           <li>Scan</li>
         </ul>
+        <p><b>Compétences acquise</b></p>
+        <div class="atout-ul-ex">
+          <li class="atout-li-ex">Autonomie</li>
+          <li class="atout-li-ex">Service Public</li>
+          <li class="atout-li-ex">Proactivité</li>
+          <li class="atout-li-ex">Organisation</li>
+          <li class="atout-li-ex">Gestion conflits</li>
+          <li class="atout-li-ex">Pédagogie</li>
+          <li class="atout-li-ex">Communication</li>
+          <li class="atout-li-ex">Adaptabiltié</li>
+          <li class="atout-li-ex">Patience</li>
+          <li class="atout-li-ex">Formation des nouveau venue</li>
+        </div>
       </div>
 
       <div class="card-experience">
         <p class="date-experience"><b>Juillet - Août 2021</b></p>
         <p class="titre-experience"><b>Auxiliaire contractuelle</b><br>Trésorerie Salon de Provence (13290)</p>
+        <p><b>Contexte de la mission</b></p>
+        <p>
+          Dans le cadre du déménagement des locaux de la trésorie de Salon de Provence, l'administration à engagner des vaccataires afin
+          d'apporter un renfort dans la répartition précise des dossiers selon leur destination et garantir une organisation optimale
+          pour faciliter le transfert des documents et minimiser les interruptions de service.
+        </p>
+        <p><b>Missions</b></p>
+        <p>
+          Archiver, pilonner et tier les dossiers des communes de Salon de Provence. En plus de la préparation des cartons, j'avais pour mission
+          d'apporter un renfort dans différentes tâches administratives.
+        </p>
+        <p><b>Tâches</b></p>
         <ul>
           <li>Archivage</li>
           <br>
@@ -185,55 +220,142 @@ include "./components/header.php";
           <br>
           <li>Prescriptions</li>
         </ul>
+        <p><b>Compétences acquise</b></p>
+        <div class="atout-ul-ex">
+          <li class="atout-li-ex">Autonomie</li>
+          <li class="atout-li-ex">Proactivité</li>
+          <li class="atout-li-ex">Organisation</li>
+          <li class="atout-li-ex">Travail d'équipe</li>
+          <li class="atout-li-ex">Adaptabiltié</li>
+        </div>
       </div>
 
       <div class="card-experience">
         <p class="date-experience"><b>Juillet 2018</b></p>
         <p class="titre-experience"><b>Stage Régie Cinémagis</b><br>Martigues (13500)</p>
+        <p><b>Contexte de la mission</b></p>
+        <p>
+          Réalisation d'un stage afin de mettre en pratique les compétences techniques et théorique apprise en formation.
+        </p>
+        <p><b>Missions</b></p>
+        <p>
+          Renfort dans l'équipé Régie du long métrage "12 Milles".
+        </p>
+        <p><b>Tâches</b></p>
         <ul>
           <li>Gestion matériel</li>
           <br>
           <li>Gestion stock alimentaire</li>
           <br>
           <li>Installation des stations de postes</li>
+          <br>
+          <li>Transport des équipes</li>
+          <br>
+          <li>Blocage des routes et contrôle des accès</li>
+          <br>
+          <li>Sécurité des équipes</li>
         </ul>
+        <p><b>Compéternces acquises</b></p>
+        <div class="atout-ul-ex">
+          <li class="atout-li-ex">Autonomie</li>
+          <li class="atout-li-ex">Proactivité</li>
+          <li class="atout-li-ex">Priorisation tâches</li>
+          <li class="atout-li-ex">Gestion conflits</li>
+          <li class="atout-li-ex">Communication</li>
+          <li class="atout-li-ex">Adaptabiltié</li>
+          <li class="atout-li-ex">Patience</li>
+          <li class="atout-li-ex">Anticipation</li>
+        </div>
       </div>
 
       <div class="card-experience">
         <p class="date-experience"><b>Juillet 2017</b></p>
         <p class="titre-experience"><b>Stage Son Cinémagis</b><br>Martigues (13500)</p>
+        <p><b>Contexte de la mission</b></p>
+        <p>
+          Réalisation d'un stage afin de mettre en pratique les compétences techniques et théorique apprise en formation.
+        </p>
+        <p><b>Missions</b></p>
+        <p>
+          Renfort dans l'équipé Son sur la série "Camping Paradis".
+        </p>
+        <p><b>Tâches</b></p>
         <ul>
           <li>Inventaire matériel</li>
           <br>
           <li>Prise et gestion son</li>
         </ul>
+        <p><b>Compétences acquises</b></p>
+        <div class="atout-ul-ex">
+          <li class="atout-li-ex">Proactivité</li>
+          <li class="atout-li-ex">Priorisation tâches</li>
+          <li class="atout-li-ex">Gestion conflits</li>
+          <li class="atout-li-ex">Patience</li>
+          <li class="atout-li-ex">Anticipation</li>
+        </div>
       </div>
 
       <div class="card-experience">
         <p class="date-experience"><b>Juillet 2016</b></p>
         <p class="titre-experience"><b>CDD Service RH à l'Institut de Recherche et du Dévelopemment</b><br>Marseille
           (13500)</p>
+        <p><b>Contexte de la mission</b></p>
+        <p>
+          Embauche saisonnier afion d’aider a la continuité du service pendant la période estivale.
+        </p>
+        <p><b>Missions</b></p>
+        <p>
+          Assurer la gestion administrative en tant que remplaçante de la secrétaire RH,
+          en prenant en charge des tâches administratives liées aux ressources humaines."
+        </p>
+        <p><b>Tâches</b></p>
         <ul>
           <li>Accueil téléphonique</li>
           <br>
           <li>Classement</li>
           <br>
-          <li>Gestion parapheurs</li>
+          <li>Gestion des parapheurs</li>
           <br>
           <li>Prise RDV</li>
           <br>
           <li>Tri du courrier</li>
         </ul>
+        <p><b>Compétences acquise</b></p>
+        <div class="atout-ul-ex">
+          <li class="atout-li-ex">Autonomie</li>
+          <li class="atout-li-ex">Proactivité</li>
+          <li class="atout-li-ex">Productivité</li>
+          <li class="atout-li-ex">Communication</li>
+          <li class="atout-li-ex">Gestion administrative</li>
+          <li class="atout-li-ex">Accueil téléphonique</li>
+          <li class="atout-li-ex">Gestion numérique</li>
+        </div>
       </div>
 
       <div class="card-experience">
         <p class="date-experience"><b>Août - Septembre 2015</b></p>
         <p class="titre-experience"><b>Auxiliaire contractuelle</b><br>Trésorerie de Martigues (13500)</p>
+        <p><b>Contexte de la mission</b></p>
+        <p>
+          Embauche saisonnier afion d’aider a la continuité du service pendant la période estivale.
+        </p>
+        <p><b>Missions</b></p>
+        <p>
+          Assurer la gestion administrative des mandats et des recettes."
+        </p>
+        <p><b>Tâches</b></p>
         <ul>
           <li>Classement et archivage des mandats Martigues et Salon de Provence</li>
           <br>
           <li>Classement et archivage des recettes Martigues et Salon de Provence</li>
         </ul>
+        <p><b>Compétences acquise</b></p>
+        <div class="atout-ul-ex">
+          <li class="atout-li-ex">Organisation</li>
+          <li class="atout-li-ex">Proactivité</li>
+          <li class="atout-li-ex">Productivité</li>
+          <li class="atout-li-ex">Travail d'équipe</li>
+        </div>
       </div>
     </section>
     <div>
